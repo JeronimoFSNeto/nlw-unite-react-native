80e28fb7d6484464c60cde876588f0cdbb74b08a
jeronimofsneto <jeronimofsneto@gmail.com>
2024-04-17 17:25:32 -0300

    Inserido o useState e onPress no Button para Alertar quem não digita o código

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 // personaliza o status bar, para que a imagem no carregamento pegue a tela inteira
-import { View, Image, StatusBar } from "react-native";
+import { View, Image, StatusBar, Alert } from "react-native";
+import { useState } from "react"
 import { Link } from "expo-router";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
@@ -9,6 +10,15 @@ import { Button } from "@/components/button";
 import colors from "tailwindcss/colors";
 
 export default function Home() {
+  const [code, setCode] = useState("")
+ 
+  //criar uma função
+  function handleAccessCredential(){
+    if(!code.trim()) {
+      return Alert.alert("Credencial", "Informe o código do ingresso!")
+    }
+  }
+
   return (
     <View className="flex-1 justify-center items-center bg-green-500 p-8">
       <StatusBar barStyle={"light-content"} />
@@ -25,10 +35,15 @@ export default function Home() {
             size={20}
             color={colors.green[200]}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          {/* <Input.Field placeholder="Código do ingresso" onChangeText={(value) => setCode(value)}/> */}
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={setCode}
+          />
         </Input>
 
-        <Button title="Acessar a credencial" />
+        <Button title="Acessar a credencial" onPress={handleAccessCredential} />
+
         <Link
           href="/register"
           className="text-gray-100 text-base font-bold text-center mt-8"
