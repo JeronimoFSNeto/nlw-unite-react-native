181bd62095f52f1e7e2cd08f7c7fdab59be1ad2c
jeronimofsneto <jeronimofsneto@gmail.com>
2024-04-18 18:01:16 -0300

    Alterando o projeto para ter outra tela inicial

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,27 +1,32 @@
-import "@/styles/global.css"
+import { Slot, Stack } from "expo-router"; //aqui devolve nossas rotas
 
-import { Slot } from "expo-router" //aqui devolve nossas rotas
+import "@/styles/global.css";
 
 import {
-    useFonts,
-    Roboto_700Bold, 
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "@/components/loading"; // import o componente que
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
     Roboto_500Medium,
     Roboto_400Regular,
-} from "@expo-google-fonts/roboto"
+  });
 
-import { Loading } from "@/components/loading" // import o componente que 
-
-export default function Layout(){
-    const [fontsLoaded] = useFonts({
-        Roboto_700Bold, 
-        Roboto_500Medium,
-        Roboto_400Regular,
-    })
-   
-    
-
-    if(!fontsLoaded){
-        return <Loading/>
-    }
-    return <Slot/>
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+  // return <Slot />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: true }} />
+      
+    </Stack>
+  );
+  
 }
